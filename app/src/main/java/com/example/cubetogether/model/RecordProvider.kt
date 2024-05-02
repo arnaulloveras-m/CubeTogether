3bd9622c8d30cae81e4fbb8d0069ce5eba31fd37
arnaulloveras <arnau.lloveras@institutvidreres.cat>
2024-05-02 11:17:33 +0200

    Records Actualitzats, nomes falten metodes

--- app/src/main/java/com/example/cubetogether/model/RecordProvider.kt
+++ app/src/main/java/com/example/cubetogether/model/RecordProvider.kt
@@ -10,20 +10,20 @@ class RecordProvider {
                 "https://www.youtube.com/watch?v=ukf2dgBWHqk"
             ),
             Record(
-                "Average 2x2 0.92 Zayn Khanani",
+                "Average 2x2 0.93 Yiheng Wang",
                 "Scrambles:" +
-                        "1: F2 R' F' U R2 U2 R F' U2 R' U2\n" +
-                        "2: R2 F2 R' U R' U2 F' U F' R' U'\n" +
-                        "3: U R U2 R' U' F' U R F' U2 F\n" +
-                        "4: F' U' F U' F2 U' R' F R F' U\n" +
-                        "5: F' U2 R2 U' R' U' F R' F' R2 F2",
-                "Soluciones: https://www.youtube.com/watch?v=-7yYeTOZNQ0",
-                "https://www.youtube.com/watch?v=-7yYeTOZNQ0"
+                        "1: U' R' U' F' U R' U2 R U2 F U2\n" +
+                        "2: U R' F U2 F2 R U' F' R U2 R'\n" +
+                        "3: R U2 R2 U' R' U2 R' F U' R U'\n" +
+                        "4: R' U' R U F U' R' U' F U2 F\n" +
+                        "5: R' F' R U2 R U' R' U' F U' F",
+                "Soluciones: https://www.youtube.com/watch?v=24c5FvI-g6Q",
+                "https://www.youtube.com/shorts/zvpqrG0UO2o\n"
 
             ),
             Record(
                 "Single 3x3 3.13 Max Park",
-                "Scramble: D R’ U2 F2 D U’ B2 R2 L’ F U’ B2 U2 F L F’ D’",
+                "Scramble: D R’ U2 F2 D U’ B2 R2 L’ F U’ B2 U2 F L F’ D’\n",
                 "Inspección: x2\n" +
                         "  Pseudo Doble X-Cross: R' D2 R' D L' U L D R' U' R D\n" +
                         "  F2L 3: L U' L'\n" +
@@ -37,10 +37,10 @@ class RecordProvider {
                 "Scrambles: \n" +
                         "Scramble 1(4.72): R2' D' R2 D2 R2 L2 D' L' B' D2 F' D U2 B' R U' F B2\n" +
                         "Scramble 2(4.72): B2' L2 D2 R2 B2 U L2 U R' D U2 R B2 R2 D2 F D U' R F'\n" +
-                        "Scramble 3(3.99):  U L2' B2 L2 D' R2 U' L2 B' L' F' U R D R2 B' R2 U F U'\n" +
+                        "Scramble 3(3.99): U L2' B2 L2 D' R2 U' L2 B' L' F' U R D R2 B' R2 U F U'\n" +
                         "Scramble 4(3.95): U2' F2 U F2 U2 R2 D2 U' L2 F D' B2 D R B U2 B2 D' R' U\n" +
                         "Scramble 5(5.99): F2' D L2 B2 U2 R2 D R2 B2 D2 F' R B2 D2 L' D2 L U R' U2",
-                "https://www.youtube.com/watch?v=2RvktW6njp4",
+                "https://ruwix.com/blog/yiheng-wang-4_48-rubiks-cube-average-record/",
                 "https://www.youtube.com/watch?v=2RvktW6njp4"
             ),
         )
